785bac10ff2b839cfb4c635064c11952141014a0
danielhumgon <danielhumgon@gmail.com>
2020-04-01 17:13:51 -0400

    feat(blockbook): Added Blockbook routes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -19,7 +19,7 @@ const Menu = (props) => (
                 <li><a href="#" className="button fit">Log In</a></li>
             </ul>
         </div>
-        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
+        <a className="close" onClick={props.onToggleMenu} href="#!">Close</a>
     </nav>
 )
 
